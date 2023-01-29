b8486da909f1e3b1ddea2f764fe87afe1e05e7c8
Name Serezhay <serezhay@mail.ru>
2023-01-29 15:19:15 +0500

    task 1.1

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,22 +1,24 @@
 
 let dead = document.getElementById('dead')
 let lost = document.getElementById('lost');
-for (let i = 1; i < document.getElementsByClassName('hole').length; i++) {
-    const cell = document.getElementById(`hole${i}`);
-    cell.onclick = () => {
-        console.log()
-        if(cell.className.includes('hole hole_has-mole')) {
+const cell = document.getElementsByClassName('hole');
+Array.from(cell).forEach((item) => {
+    item.onclick = () => {
+        if(item.className.includes('hole hole_has-mole')) {
             dead.textContent++;
             if( dead.textContent == 10) {
                 alert('Победа');
-                dead.textContent = 0
             }
         } else {
             lost.textContent++;
             if(lost.textContent == 5) {
                 alert('Поражение');
-                lost.textContent = 0;
             }
         }
-    };
-}
+        if(dead.textContent == 10 || lost.textContent == 5) {
+            dead.textContent = 0
+            lost.textContent = 0;
+        }
+    }
+    
+})
